f223eb791d1447480da5fda8b0464dca5b4b4d74
Sai Nagarjuna <95867023+sainag6966@users.noreply.github.com>
2024-03-01 10:21:17 +0530

    added bootstrap elem

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,17 @@
 import styles from "./header.module.css";
+import { Button } from "react-bootstrap";
 
 function Header() {
-  return <main className={styles.header}>This is the header component</main>;
+  return (
+    <main className={styles.header}>
+      <p>This is the header component</p>
+      <Button as="a" variant="primary">
+        Button as link
+      </Button>
+      <Button as="a" variant="success">
+        Button as link
+      </Button>
+    </main>
+  );
 }
 export default Header;
