e47589617029121d534f3cfe1a7b6dc5596d572f
Sai Nagarjuna <sai@cda.group>
2024-03-03 12:29:31 +0530

    commit check to new account

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,7 @@
 import styles from "./footer.module.css";
 
 function Footer() {
-  return <main className={styles.header}>This is the footer component</main>;
+  return <main className={styles.header}>This is the footer component and a commit check to new account
+  </main>;
 }
 export default Footer;
