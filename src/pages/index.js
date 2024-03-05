f90456f808afbf51dae03975e6d7c28fff5ad39d
Chethan HP <chethan@cda.group>
2024-03-05 16:20:57 +0530

    Chages on all Bocks

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import FlameBtn from "@/reusbleComponents/FlameBtn";
 import TitleAndTextCentre from "@/components/ContentBlocks/TitleAndTextCentre";
 import HeaderBanner from "@/components/ContentBlocks/HeaderBanner";
 import UspBlock from "@/components/ContentBlocks/UspBlock";
+import TwoAdBlocks from "@/components/ContentBlocks/TwoAdBlocks";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -40,6 +41,7 @@ export default function Home() {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id."
           }
         />
+        <TwoAdBlocks />
       </main>
     </>
   );
