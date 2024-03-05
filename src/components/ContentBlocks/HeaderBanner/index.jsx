e299c9fa6bbd222afb860a83a11ff5090db11e0e
Sai Nagarjuna <sai@cda.group>
2024-03-05 11:02:15 +0530

    header banner text changes

--- src/components/ContentBlocks/HeaderBanner/index.jsx
+++ src/components/ContentBlocks/HeaderBanner/index.jsx
@@ -1,12 +1,26 @@
 import FlameImage from '@/reusbleComponents/FlameImage'
+import FlameBtn from '@/reusbleComponents/FlameBtn'
 import styles from './headerBanner.module.css'
 
 function HeaderBanner() {
     const bannerImage = '/Images/headerBannerMweb.svg'
+    const buttonColor = 'var(--color-secondary)'
+    const textColor = 'var(--color-primary)'
+    const buttonText = 'About Flame PRO'
+
+    const handleButtonClick = () => { }
+
     return <section className={styles.main}>
         <figure className={styles.bannerImage}>
             <FlameImage src={bannerImage} alt='bannerImage' />
         </figure>
+        <section className={styles.headerTextBlock}>
+            <h1 className={styles.heading}>Heading Goes Here On One Or Two Lines</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <section className={styles.btnSection}>
+                <FlameBtn color={buttonColor} textColor={textColor} text={buttonText} isLoadState={false} btnFunction={handleButtonClick} />
+            </section>
+        </section>
     </section>
 }
 export default HeaderBanner
