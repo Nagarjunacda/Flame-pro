a51ecf0da4f76112b175a91df582cd42fec7c239
Sai Nagarjuna <sai@cda.group>
2024-03-12 09:23:02 +0530

    minor changes for testimonial block

--- src/components/ContentBlocks/TwoAdBlocks/index.jsx
+++ src/components/ContentBlocks/TwoAdBlocks/index.jsx
@@ -1,21 +1,21 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import FlameImage from "@/reusbleComponents/FlameImage";
-import Style from "@/components/ContentBlocks/TwoAdBlocks/twoblock.module.css";
 import ButtonStyleTwo from "@/reusbleComponents/ButtonStyleTwo";
+import Style from "./twoblock.module.css";
 
 function TwoAdBlocks({ trayData }) {
-  function btnFunction() {}
+  function btnFunction() { }
   return (
     <section className={Style.twoblockMain}>
-      <div className={Style.mainContainer}>
-        <div className={Style.blockOne}>
+      <section className={Style.mainContainer}>
+        <section className={Style.blockOne}>
           <figure className={Style.figOne}>
             <FlameImage src={"/images/block1.jpg"} />
           </figure>
-          <div className={Style.overlay}></div>
-          <div className={Style.twoBlockData}>
-            <div className={Style.logoHolder}>
+          <section className={Style.overlay}></section>
+          <section className={Style.twoBlockData}>
+            <section className={Style.logoHolder}>
               <figure className={Style.logosImg}>
                 <FlameImage src={"/images/fireLogo.svg"} alt={"fireLogo"} />
               </figure>
@@ -24,16 +24,16 @@ function TwoAdBlocks({ trayData }) {
                 textColor="var( --color-secondary)"
                 btnFunction={btnFunction}
               />
-            </div>
-          </div>
-        </div>
-        <div className={Style.blockTwo}>
+            </section>
+          </section>
+        </section>
+        <section className={Style.blockTwo}>
           <figure className={Style.figTwo}>
             <FlameImage src={"/images/block2.jpg"} />
           </figure>
-          <div className={Style.overlay}></div>
-          <div className={Style.twoBlockData}>
-            <div className={Style.logoHolder}>
+          <section className={Style.overlay}></section>
+          <section className={Style.twoBlockData}>
+            <section className={Style.logoHolder}>
               <figure className={Style.logosImg}>
                 <FlameImage src={"/images/fireLogo.svg"} alt={"fireLogo"} />
               </figure>
@@ -42,10 +42,10 @@ function TwoAdBlocks({ trayData }) {
                 textColor="var( --color-secondary)"
                 btnFunction={btnFunction}
               />
-            </div>
-          </div>
-        </div>
-      </div>
+            </section>
+          </section>
+        </section>
+      </section>
     </section>
   );
 }
