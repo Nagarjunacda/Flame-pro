6cc399e4f31ec5ddc070c8cbecc1c9fd8ab2dfff
Sai Nagarjuna <sai@cda.group>
2024-03-20 12:46:13 +0530

    grid four category block

--- src/components/ContentBlocks/FourCategoryBlock/index.jsx
+++ src/components/ContentBlocks/FourCategoryBlock/index.jsx
@@ -1,8 +1,11 @@
 import SliderComp from "@/reusbleComponents/SliderComp";
 import { Container, Row, Col } from 'react-bootstrap';
 import styles from './fourCategoryBlock.module.css'
+import { useMediaQuery } from "react-responsive";
+import FourCategoryCard from "@/components/Cards/FourCategoryCard";
 
 function FourCategoryBlock({ trayData, categories }) {
+    const isDesktop = useMediaQuery({ query: '(min-width:900px)' })
     const trayTitle = trayData?.acf_fc_layout
     const categoryType = trayData?.select_category
     const blockTitle = trayData?.title
@@ -12,7 +15,16 @@ function FourCategoryBlock({ trayData, categories }) {
 
     return <section className={styles.mainCont}>
         {blockTitle && <p className={styles.title}>{blockTitle}</p>}
-        <section className={styles.slider}><SliderComp data={productList} title={trayTitle} slidesToShow={4} /></section>
+        {!isDesktop ? <section className={styles.slider}><SliderComp data={productList} title={trayTitle} slidesToShow={4} /></section> :
+            <Container fluid>
+                <Row>
+                    {productList.map((item, index) => {
+                        return <Col key={index} md={3} className={styles.col}>
+                            <FourCategoryCard data={item} />
+                        </Col>
+                    })}
+                </Row>
+            </Container>}
     </section>
 }
 export default FourCategoryBlock
