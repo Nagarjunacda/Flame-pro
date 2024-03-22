09db24b70582b2528f30a87cc020d6066dcf6d9f
Chethan HP <chethan@cda.group>
2024-03-22 18:12:28 +0530

    added thank you page

--- src/components/ContentBlocks/BasketTitleBlock/index.jsx
+++ src/components/ContentBlocks/BasketTitleBlock/index.jsx
@@ -1,18 +1,11 @@
 import styles from "./basketTitle.module.css";
-const BasketTitleBlock = () => {
+const BasketTitleBlock = ({ title, subHeading, description }) => {
   return (
     <section className={styles.bastetSection}>
       <div className={styles.basketcontent}>
-        <h2>Your Quote Basket</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id.
-        </p>
+        <h2>{title}</h2>
+        {subHeading && <h3>{subHeading}</h3>}
+        <p>{description}</p>
       </div>
     </section>
   );
--- src/pages/thank-you/index.jsx
+++ src/pages/thank-you/index.jsx
@@ -4,12 +4,12 @@ const Basket = () => {
   return (
     <>
       <BasketTitleBlock
-        title={"Your Quote Basket"}
+        title={"Thank You For Submitting Your Quote"}
         description={
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id."
         }
+        subHeading={"Reference No. 12345"}
       />
-      <BasketItems />
     </>
   );
 };
