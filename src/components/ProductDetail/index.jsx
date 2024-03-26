c56e04de8966b945b365decd970ddb76f327f995
Sai Nagarjuna <sai@cda.group>
2024-03-26 23:26:30 +0530

    breadcrumbs added

--- src/components/ProductDetail/index.jsx
+++ src/components/ProductDetail/index.jsx
@@ -3,14 +3,19 @@ import NonceContext from "@/context/NonceContext";
 import { useCartData } from "@/context/CartContext";
 import { handlePostRequests } from "@/utils/handlePostCalls"
 import { addToCartUrl } from "@/utils/urls"
+import Breadcrumbs from "../BreadCrumbs";
+import styles from './productDetail.module.css'
 
 function ProductDetail({ productData }) {
-    const nonceVal = useContext(NonceContext)
     const { setTriggerUpdate } = useCartData();
+    const nonceVal = useContext(NonceContext)
+    const productId = productData?.id
+    console.log(productData, '!!')
+
     const handleAddCart = async () => {
         const data = {
-            "id": 3727,
-            "variation": [
+            id: productId,
+            variation: [
                 {
                     "attribute": "Gloves Size",
                     "value": "10"
@@ -23,6 +28,8 @@ function ProductDetail({ productData }) {
             setTriggerUpdate(true)
         }
     }
-    return <button onClick={handleAddCart}>Add to Cart</button>
+    return <main className={styles.mainCont}>
+        <Breadcrumbs />
+    </main>
 }
 export default ProductDetail
