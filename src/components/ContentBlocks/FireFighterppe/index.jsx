d4ad4679a20031666503c5877f2841dd390159f1
Sai Nagarjuna <sai@cda.group>
2024-03-27 17:18:17 +0530

    header and headerbanner dweb mweb dont

--- src/components/ContentBlocks/FireFighterppe/index.jsx
+++ src/components/ContentBlocks/FireFighterppe/index.jsx
@@ -9,6 +9,7 @@ function FireFighterppe({ selectedNavItem, handleOverlayClose }) {
     const heading = selectedNavItem?.title
     const childItems = selectedNavItem?.child_items
     const isFireFighting = selectedNavItem?.title === 'Firefighting PPE'
+    const isDefencePro = selectedNavItem?.title === 'Defence Procurement'
     const backArrow = '/Images/backArrow.svg'
     const isDesktop = useMediaQuery({ query: '(min-width:900px)' })
     const isLargeScreen = useMediaQuery({ query: '(min-width:1280px)' })
@@ -24,12 +25,12 @@ function FireFighterppe({ selectedNavItem, handleOverlayClose }) {
                 </section>
                 <p className={styles.backText}>Back</p>
             </section>
-            <p className={styles.headingText}>{heading}</p>
+            <p className={isDefencePro ? styles.headingTextMar : styles.headingText}>{heading}</p>
         </section>}
         <section className={styles.subContainer}>
             <section className={isFireFighting ? styles.childHeadingPpe : styles.childHeading}>
                 {childItems && childItems.map((item, index) => {
-                    return <section className={styles.childCont}><section key={index}>{item?.title}</section>
+                    return <section className={styles.childCont}><section className={isFireFighting && !isDesktop ? styles.subContTitle : null} key={index}>{item?.title}</section>
                         {item.child_items && item.child_items.map((childItem, index) => {
                             return <section className={styles.innerChild}>
                                 <section key={index}>{childItem?.title}</section>
