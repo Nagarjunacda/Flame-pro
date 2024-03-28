dd283d7e89b333603a6042762a9b969ab531eeec
Sai Nagarjuna <sai@cda.group>
2024-03-28 23:49:04 +0530

    form dropdown and checkbox added

--- src/components/SignUpForm/CheckBoxWithText/index.jsx
+++ src/components/SignUpForm/CheckBoxWithText/index.jsx
@@ -1,7 +1,17 @@
+import FlameImage from '@/reusbleComponents/FlameImage';
 import styles from '../signUpForm.module.css'
-function CheckBoxWithText({ text }) {
+
+function CheckBoxWithText({ text, setIsChecked, isChecked }) {
+    const checkIconSrc = '/Images/blackTickIcon.svg'
+
+    const handleCheckBox = () => {
+        setIsChecked(!isChecked)
+    }
+
     return <section className={styles.checkBoxCont}>
-        <section className={styles.checkBox}></section>
+        <section className={styles.checkBox} onClick={handleCheckBox}>
+            {isChecked && <FlameImage src={checkIconSrc} alt='check' />}
+        </section>
         <p className={styles.checkBoxText}>{text}</p>
     </section>
 }
--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { use, useState } from 'react';
 import CheckBoxWithText from './CheckBoxWithText';
 import FlameBtn from '@/reusbleComponents/FlameBtn';
 import FlameImage from '@/reusbleComponents/FlameImage';
@@ -8,9 +8,14 @@ import styles from './signUpForm.module.css'
 function SignUpForm({ isFromFooter, text, heading, formFields }) {
     const [formData, setFormData] = useState({});
     const [errors, setErrors] = useState({});
+    const [areaOfInt, setAreaOfInt] = useState('Area Of Interest')
+    const [isDropdownOpen, setIsDropDownOpen] = useState(false)
+    const [isChecked, setIsChecked] = useState(false)
     const btnColor = 'var(--color-secondary)'
     const textColor = 'var(--color-primary)'
     const checkBoxText = 'I consent to email marketing'
+    const areaOfInterests = ['Fire Fighting Ppe', 'Defence Procurement']
+    const arrowImgSrc = isDropdownOpen ? '/Images/upWhiteArrow.svg' : '/Images/downWhiteArrow.svg'
 
     const handleChange = (e, fieldName) => {
         console.log(errors, formFields, '!!')
@@ -56,6 +61,15 @@ function SignUpForm({ isFromFooter, text, heading, formFields }) {
         e.preventDefault();
     };
 
+    const handleDropdown = () => {
+        setIsDropDownOpen(!isDropdownOpen)
+    }
+
+    const handleInterestSel = (area) => {
+        setAreaOfInt(area)
+        setIsDropDownOpen(false)
+    }
+
     return <section className={styles.signUpCont}>
         <p className={isFromFooter ? styles.headingText : styles.headingTextBlock}>{heading}</p>
         {text && <p className={styles.text}>{renderHTML(text)}</p>}
@@ -63,22 +77,27 @@ function SignUpForm({ isFromFooter, text, heading, formFields }) {
             {formFields?.map((fieldName) => (
                 <div key={fieldName} className={styles.formDiv}>
                     <section className={styles.formInput}>
-                        <input
-                            type="text"
-                            id={fieldName?.section1}
-                            name={fieldName?.section1}
-                            className={fieldName?.section1 === 'Area Of Interest*' ? styles.areaInterestInput : styles.textInput}
-                            placeholder={fieldName?.section1}
-                            onChange={(e) => handleChange(e, fieldName?.section1)}
-                            value={formData[fieldName?.section1] || ''}
-                        />
+                        {fieldName?.section1 !== 'Area Of Interest*' ?
+                            <input
+                                type="text"
+                                id={fieldName?.section1}
+                                name={fieldName?.section1}
+                                className={fieldName?.section1 === 'Area Of Interest*' ? styles.areaInterestInput : styles.textInput}
+                                placeholder={fieldName?.section1}
+                                onChange={(e) => handleChange(e, fieldName?.section1)}
+                                value={formData[fieldName?.section1] || ''}
+                            /> : <section className={styles.areaInterestInput} onClick={handleDropdown}>{areaOfInt}</section>}
                         {errors[fieldName?.section1] && <p className={styles.errorMsg}>{errors[fieldName?.section1]}</p>}
                         {fieldName?.section1 === 'Area Of Interest*' && <section className={styles.downArrow}>
-                            <FlameImage src={'/Images/downWhiteArrow.svg'} alt='arrow' />
+                            <FlameImage src={arrowImgSrc} alt='arrow' />
                         </section>}
+                        {fieldName?.section1 === 'Area Of Interest*' && isDropdownOpen &&
+                            <section className={styles.areaOfInt}>{areaOfInterests.map((area) => {
+                                return <section className={styles.interestOptions} onClick={() => { handleInterestSel(area) }}>{area}</section>
+                            })}</section>}
                     </section>
                     {fieldName.section2 && <section className={styles.formInput}>
-                        <input
+                        {fieldName?.section2 !== 'Area Of Interest*' ? <input
                             type="text"
                             id={fieldName?.section2}
                             name={fieldName?.section2}
@@ -86,9 +105,9 @@ function SignUpForm({ isFromFooter, text, heading, formFields }) {
                             placeholder={fieldName?.section2}
                             onChange={(e) => handleChange(e, fieldName?.section2)}
                             value={formData[fieldName?.section2] || ''}
-                        />
+                        /> : <section className={styles.areaInterestInput}>Area Of Interest</section>}
                         {errors[fieldName?.section2] && <p className={styles.errorMsg}>{errors[fieldName?.section2]}</p>}
-                        {fieldName?.section1 === 'Area Of Interest*' && <section className={styles.downArrow}>
+                        {fieldName?.section2 === 'Area Of Interest*' && <section className={styles.downArrow}>
                             <FlameImage src={'/Images/downWhiteArrow.svg'} alt='arrow' />
                         </section>}
                     </section>}
@@ -96,7 +115,7 @@ function SignUpForm({ isFromFooter, text, heading, formFields }) {
             ))}
         </form>
         <section className={styles.checkBoxAndText}>
-            <section className={styles.checkBoxSection}><CheckBoxWithText text={checkBoxText} /></section>
+            <section className={styles.checkBoxSection}><CheckBoxWithText text={checkBoxText} isChecked={isChecked} setIsChecked={setIsChecked} /></section>
         </section >
         <section className={styles.cta}><FlameBtn color={btnColor} text={'Sign Up'} textColor={textColor} isLoadState={false} btnFunction={handleButtonClick} /></section>
     </section>
