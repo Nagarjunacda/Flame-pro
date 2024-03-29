45440fef2d0837b63226ad86bd707e313dc25f15
Chethan HP <chethan@cda.group>
2024-03-29 11:53:42 +0530

    fixed more fonts

--- src/components/ContentBlocks/FourCategoryBlock/index.jsx
+++ src/components/ContentBlocks/FourCategoryBlock/index.jsx
@@ -1,30 +1,39 @@
 import SliderComp from "@/reusbleComponents/SliderComp";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 import { useMediaQuery } from "react-responsive";
 import FourCategoryCard from "@/components/Cards/FourCategoryCard";
-import styles from './fourCategoryBlock.module.css'
+import styles from "./fourCategoryBlock.module.css";
 
 function FourCategoryBlock({ trayData, categories }) {
-    const isDesktop = useMediaQuery({ query: '(min-width:900px)' })
-    const trayTitle = trayData?.acf_fc_layout
-    const categoryType = trayData?.select_category
-    const blockTitle = trayData?.title
-    const productList = categories?.filter((item) => {
-        return item?.category === categoryType
-    })
+  const isDesktop = useMediaQuery({ query: "(min-width:900px)" });
+  const trayTitle = trayData?.acf_fc_layout;
+  const categoryType = trayData?.select_category;
+  const blockTitle = trayData?.title;
+  const productList = categories?.filter((item) => {
+    return item?.category === categoryType;
+  });
 
-    return <section className={styles.mainCont}>
-        {blockTitle && <p className={styles.title}>{blockTitle}</p>}
-        {!isDesktop ? <section className={styles.slider}><SliderComp data={productList} title={trayTitle} slidesToShow={4} /></section> :
-            <Container fluid>
-                <Row>
-                    {productList?.map((item, index) => {
-                        return <Col key={index} md={3} className={styles.col}>
-                            <FourCategoryCard data={item} />
-                        </Col>
-                    })}
-                </Row>
-            </Container>}
+  return (
+    <section className={styles.mainCont}>
+      {blockTitle && <h5 className={styles.title}>{blockTitle}</h5>}
+      {!isDesktop ? (
+        <section className={styles.slider}>
+          <SliderComp data={productList} title={trayTitle} slidesToShow={4} />
+        </section>
+      ) : (
+        <Container fluid>
+          <Row>
+            {productList?.map((item, index) => {
+              return (
+                <Col key={index} md={3} className={styles.col}>
+                  <FourCategoryCard data={item} />
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      )}
     </section>
+  );
 }
-export default FourCategoryBlock
+export default FourCategoryBlock;
--- src/components/ContentBlocks/FullWidthAdBlock/index.jsx
+++ src/components/ContentBlocks/FullWidthAdBlock/index.jsx
@@ -1,33 +1,41 @@
-import FlameImage from '@/reusbleComponents/FlameImage'
-import FlameBtn from '@/reusbleComponents/FlameBtn'
-import { renderHTML } from '@/utils/htmlString'
-import styles from './fullWidthAdBlock.module.css'
+import FlameImage from "@/reusbleComponents/FlameImage";
+import FlameBtn from "@/reusbleComponents/FlameBtn";
+import { renderHTML } from "@/utils/htmlString";
+import styles from "./fullWidthAdBlock.module.css";
 
 function FullWidthAdBlock({ trayData }) {
-    const imgSrc = trayData?.image
-    const text = trayData?.text
-    const buttonTitle = trayData?.button_title
-    const title = trayData?.title
-    const buttonColor = 'var(--color-secondary)'
-    const btnTextColor = 'var(--color-primary)'
-    const buttonText = trayData?.button_title
+  const imgSrc = trayData?.image;
+  const text = trayData?.text;
+  const buttonTitle = trayData?.button_title;
+  const title = trayData?.title;
+  const buttonColor = "var(--color-secondary)";
+  const btnTextColor = "var(--color-primary)";
+  const buttonText = trayData?.button_title;
 
-    const btnClick = () => { }
+  const btnClick = () => {};
 
-    return <section className={styles.mainCont}>
-        <section className={styles.imageStyles}>
-            <FlameImage src={imgSrc} alt='image' />
+  return (
+    <section className={styles.mainCont}>
+      <section className={styles.imageStyles}>
+        <FlameImage src={imgSrc} alt="image" />
+      </section>
+      <section className={styles.overlay}></section>
+      <section className={styles.form}>
+        <section className={styles.innerSec}>
+          <h5 className={styles.title}>{title}</h5>
+          <p className={styles.desc}>{renderHTML(text)}</p>
         </section>
-        <section className={styles.overlay}></section>
-        <section className={styles.form}>
-            <section className={styles.innerSec}>
-                <p className={styles.title}>{title}</p>
-                <p className={styles.desc}>{renderHTML(text)}</p>
-            </section>
-            <section className={styles.btnSection}>
-                <FlameBtn color={buttonColor} text={buttonText} textColor={btnTextColor} isLoadState={false} btnFunction={btnClick} />
-            </section>
+        <section className={styles.btnSection}>
+          <FlameBtn
+            color={buttonColor}
+            text={buttonText}
+            textColor={btnTextColor}
+            isLoadState={false}
+            btnFunction={btnClick}
+          />
         </section>
+      </section>
     </section>
+  );
 }
-export default FullWidthAdBlock
+export default FullWidthAdBlock;
