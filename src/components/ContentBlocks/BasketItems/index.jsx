e61174630ff707b11b2cd34e7742b75bb3821d27
Chethan HP <chethan@cda.group>
2024-04-03 17:09:48 +0530

    fixed basket page

--- src/components/ContentBlocks/BasketItems/index.jsx
+++ src/components/ContentBlocks/BasketItems/index.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { useContext } from "react";
+import NonceContext from "@/context/NonceContext";
+import { useCartData } from "@/context/CartContext";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -6,11 +10,18 @@ import style from "./basketItems.module.css";
 import ButtonStyleTwo from "@/reusbleComponents/ButtonStyleTwo";
 import InputGroup from "react-bootstrap/InputGroup";
 import Form from "react-bootstrap/Form";
+import Spinner from "react-bootstrap/Spinner";
+import TitleAndTextCentre from "../TitleAndTextCentre";
 
-import { useState } from "react";
-const BasketItems = ({ cartData }) => {
+const BasketItems = () => {
+  const nonceVal = useContext(NonceContext);
+  const { cartData } = useCartData();
   const [showTextBox, setShowTextBox] = useState(false);
   const [quanityValue, setquanityValue] = useState("");
+  const emptyBasket = {
+    title: "Basket Empty",
+    button_title: "Shop All",
+  };
   console.log(cartData, "!!!!!!!!!");
   function btnFunction() {}
   function handleEditQuote() {
@@ -18,96 +29,102 @@ const BasketItems = ({ cartData }) => {
   }
   function quanityInput(e) {}
   return (
-    <section className={style.backetProducts}>
-      <div className="d-none d-sm-none d-lg-block">
-        <header className={style.headerRow}>
-          <Row className="d-flex justify-content-start align-items-center">
-            <Col lg={2} sm={4} xs={4}>
-              <h3>Product</h3>
-            </Col>
-            <Col lg={10} sm={8} xs={8}>
-              <Row className="">
-                <Col lg={4} sm={12} xs={12}></Col>
-                <Col lg={2} sm={12} xs={12}>
-                  <h3>Quantity</h3>
+    <>
+      {cartData?.items?.length ? (
+        <section className={style.backetProducts}>
+          <div className="d-none d-sm-none d-lg-block">
+            <header className={style.headerRow}>
+              <Row className="d-flex justify-content-start align-items-center">
+                <Col lg={2} sm={4} xs={4}>
+                  <h3>Product</h3>
                 </Col>
-                <Col lg={3} sm={12} xs={12}></Col>
-                <Col lg={3} sm={12} xs={12}></Col>
+                <Col lg={10} sm={8} xs={8}>
+                  <Row className="">
+                    <Col lg={4} sm={12} xs={12}></Col>
+                    <Col lg={2} sm={12} xs={12}>
+                      <h3>Quantity</h3>
+                    </Col>
+                    <Col lg={3} sm={12} xs={12}></Col>
+                    <Col lg={3} sm={12} xs={12}></Col>
+                  </Row>
+                </Col>
               </Row>
-            </Col>
-          </Row>
-        </header>
-      </div>
+            </header>
+          </div>
 
-      {/* Header ends */}
-      {cartData?.items?.map((data, index) => (
-        <div key={index}>
-          <Row className={style.BasketItemsRow}>
-            <Col lg={2} sm={4} xs={4}>
-              <figure className={style.productImage}>
-                <FlameImage src={data.images[0].src} alt={"Product"} />
-              </figure>
-            </Col>
-            <Col lg={10} sm={8} xs={8}>
-              <Row className="">
-                <Col lg={4} sm={12} xs={12}>
-                  <h3 className="mb-xs-4 mb-sm-4 mb-4 mb-lg-0 mb-md-4">
-                    {data.name}
-                  </h3>
+          {/* Header ends */}
+          {cartData?.items?.map((data, index) => (
+            <div key={index}>
+              <Row className={style.BasketItemsRow}>
+                <Col lg={2} sm={4} xs={4}>
+                  <figure className={style.productImage}>
+                    <FlameImage src={data.images[0].src} alt={"Product"} />
+                  </figure>
                 </Col>
-                <Col
-                  lg={2}
-                  sm={12}
-                  xs={12}
-                  className="mb-xs-4 mb-sm-4 mb-4 mb-lg-0 mb-md-4"
-                >
-                  {" "}
-                  {showTextBox ? (
-                    <InputGroup>
-                      <Form.Control
-                        type="text"
-                        aria-label="Quantity"
-                        aria-describedby="Quantity"
-                        value={data.quantity}
-                        onChange={quanityInput}
+                <Col lg={10} sm={8} xs={8}>
+                  <Row className="">
+                    <Col lg={4} sm={12} xs={12}>
+                      <h3 className="mb-xs-4 mb-sm-4 mb-4 mb-lg-0 mb-md-4">
+                        {data.name}
+                      </h3>
+                    </Col>
+                    <Col
+                      lg={2}
+                      sm={12}
+                      xs={12}
+                      className="mb-xs-4 mb-sm-4 mb-4 mb-lg-0 mb-md-4"
+                    >
+                      {" "}
+                      {showTextBox ? (
+                        <InputGroup>
+                          <Form.Control
+                            type="text"
+                            aria-label="Quantity"
+                            aria-describedby="Quantity"
+                            value={data.quantity}
+                            onChange={quanityInput}
+                          />
+                          <InputGroup.Text id="btnGroupAddon">
+                            &#10004;
+                          </InputGroup.Text>
+                        </InputGroup>
+                      ) : (
+                        data.quantity
+                      )}
+                    </Col>
+                    <Col
+                      lg={3}
+                      sm={12}
+                      xs={12}
+                      className="mb-xs-4 mb-sm-4 mb-4 mb-lg-0 mb-md-4"
+                    >
+                      <ButtonStyleTwo
+                        text={"Remove Product"}
+                        textColor="var( --color-primary)"
+                        btnFunction={btnFunction}
+                        btnIcon={"/Images/deleteIcon.svg"}
                       />
-                      <InputGroup.Text id="btnGroupAddon">
-                        &#10004;
-                      </InputGroup.Text>
-                    </InputGroup>
-                  ) : (
-                    data.quantity
-                  )}
-                </Col>
-                <Col
-                  lg={3}
-                  sm={12}
-                  xs={12}
-                  className="mb-xs-4 mb-sm-4 mb-4 mb-lg-0 mb-md-4"
-                >
-                  <ButtonStyleTwo
-                    text={"Remove Product"}
-                    textColor="var( --color-primary)"
-                    btnFunction={btnFunction}
-                    btnIcon={"/Images/deleteIcon.svg"}
-                  />
-                </Col>
-                <Col lg={3} sm={12} xs={12}>
-                  <ButtonStyleTwo
-                    text={showTextBox ? "Cancel" : "Edit Quote"}
-                    textColor="var( --color-primary)"
-                    btnFunction={handleEditQuote}
-                    btnIcon={"/Images/editIcon.svg"}
-                  />
+                    </Col>
+                    <Col lg={3} sm={12} xs={12}>
+                      <ButtonStyleTwo
+                        text={showTextBox ? "Cancel" : "Edit Quote"}
+                        textColor="var( --color-primary)"
+                        btnFunction={handleEditQuote}
+                        btnIcon={"/Images/editIcon.svg"}
+                      />
+                    </Col>
+                  </Row>
                 </Col>
               </Row>
-            </Col>
-          </Row>
-          <hr className={style.horizontalLine} />
-        </div>
-      ))}
-      {/* items */}
-    </section>
+              <hr className={style.horizontalLine} />
+            </div>
+          ))}
+          {/* items */}
+        </section>
+      ) : (
+        <TitleAndTextCentre trayData={emptyBasket} />
+      )}
+    </>
   );
 };
 
--- src/pages/basket/index.jsx
+++ src/pages/basket/index.jsx
@@ -6,23 +6,35 @@ import BasketTitleBlock from "@/components/ContentBlocks/BasketTitleBlock";
 import BasketItems from "@/components/ContentBlocks/BasketItems";
 import Breadcrumbs from "@/components/BreadCrumbs";
 import { handleServerSideProps } from "@/utils/handleServerSideData";
+import { basketPageUrl } from "@/utils/urls";
+import RenderTrays from "@/components/RenderTrays";
 
-const Basket = (props) => {
+const Basket = ({ data }) => {
   const nonceVal = useContext(NonceContext);
   const { cartData } = useCartData();
-  console.log(props);
 
+  const trayData = data?.acf?.content_blocks;
+
+  // return (
+  //   <>
+  //     <Breadcrumbs isPadding={true} />
+  //     <BasketTitleBlock
+  //       title={"Your Quote Basket"}
+  //       description={
+  //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id."
+  //       }
+  //     />
+  //     {cartData?.items?.length ? <BasketItems cartData={cartData} /> : null}
+  //   </>
+  // );
   return (
-    <>
-      <Breadcrumbs isPadding={true} />
-      <BasketTitleBlock
-        title={"Your Quote Basket"}
-        description={
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id."
-        }
-      />
-      {cartData?.items?.length ? <BasketItems cartData={cartData} /> : null}
-    </>
+    <main>
+      {trayData ? (
+        <RenderTrays trayData={trayData} />
+      ) : (
+        <p>This Page Under Development</p>
+      )}
+    </main>
   );
 };
 
