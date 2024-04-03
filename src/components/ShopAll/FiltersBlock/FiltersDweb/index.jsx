f406beafc034d6763faa9236b53e57088e4cb8dd
Sai Nagarjuna <sai@cda.group>
2024-04-03 15:28:40 +0530

    filters one iteration working fine

--- src/components/ShopAll/FiltersBlock/FiltersDweb/index.jsx
+++ src/components/ShopAll/FiltersBlock/FiltersDweb/index.jsx
@@ -2,18 +2,25 @@ import { useState } from 'react'
 import CheckBoxWithText from '@/components/SignUpForm/CheckBoxWithText'
 import styles from '../filters.module.css'
 
-function FiltersDweb({ filtersData }) {
+function FiltersDweb({ filtersData, getFilteredProducts }) {
     const [isChecked, setIsChecked] = useState(false)
     const [selectedItem, setSelectedItem] = useState({})
     const [itemsArray, setItemsArray] = useState([])
 
-    const handleItemClick = (item) => {
-        if (itemsArray?.includes(item?.title)) {
-            setItemsArray(itemsArray.filter(title => title !== item?.title));
-            return
+    const handleItemClick = async (item) => {
+        const isFilterUnchecked = itemsArray.some(filter => filter?.title === item?.title);
+        let updatedArray;
+
+        if (isFilterUnchecked) {
+            updatedArray = itemsArray.filter(filter => filter?.title !== item?.title);
+        } else {
+            updatedArray = [...itemsArray, item];
         }
-        setSelectedItem(item)
-        itemsArray.push(item?.title)
+        setItemsArray(updatedArray);
+        getFilteredProducts(updatedArray);
+        setSelectedItem(item);
+        // const url = 'https://flameprodev.cda-development3.co.uk/cms/wp-json/custom/v1/products?application=185&per_page=20&page=1';
+        // const { data, error, headers } = await handleServerSideProps(url);
     }
 
     return <section className={styles.mainCont}>
@@ -24,7 +31,7 @@ function FiltersDweb({ filtersData }) {
                     <h4 className={styles.filterTitle}>{category?.taxonomy_title}</h4>
                     <section className={styles.categorySection}>
                         {category?.terms?.map((categoryTitle, index) => {
-                            const isItemChecked = itemsArray?.includes(categoryTitle?.title)
+                            const isItemChecked = itemsArray?.some(item => item.title === categoryTitle?.title);
                             return <section key={index} onClick={() => { handleItemClick(categoryTitle) }}><CheckBoxWithText key={index} text={categoryTitle?.title} isChecked={isItemChecked} setIsChecked={setIsChecked} isDarkMode /></section>
                         })}
                     </section>
