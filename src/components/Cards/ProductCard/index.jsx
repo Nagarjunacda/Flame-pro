67b162fd23f6423487854e7003a4ba848bd88df1
Sai Nagarjuna <sai@cda.group>
2024-04-04 10:01:39 +0530

    sale label on products

--- src/components/Cards/ProductCard/index.jsx
+++ src/components/Cards/ProductCard/index.jsx
@@ -8,10 +8,12 @@ function ProductCard({ product }) {
     const cardTitle = product?.name
     const slug = product?.slug
     const productId = product?.id
+    const isOnSale = product?.on_sale
     const productUrl = `/shop-all/${slug}/${productId}`
 
     return <Link href={productUrl}><Card className={styles?.cardCont}>
         <Card.Img variant="top" src={cardImage} className={styles.cardImg} />
+        {isOnSale && <section className={styles.saleLabel}>Sale</section>}
         <Card.Body className={styles.cardBody}>
             <Card.Title className={styles.cardTitle}>{cardTitle}</Card.Title>
         </Card.Body>
