988cb3734429fde1d6eb24623055c4a3ef16bd82
Chethan HP <chethan@cda.group>
2024-04-16 14:58:48 +0530

    image right text left btn added

--- src/components/ContentBlocks/ImageRightTextLeft/index.jsx
+++ src/components/ContentBlocks/ImageRightTextLeft/index.jsx
@@ -2,32 +2,47 @@ import FlameImage from "@/reusbleComponents/FlameImage";
 import FlameBtn from "@/reusbleComponents/FlameBtn";
 import { renderHTML } from "@/utils/htmlString";
 import styles from "./imageRightTextLeft.module.css";
+import Link from "next/link";
 
 function ImageRightTextLeft({ trayData }) {
+  console.log(trayData, "trayDatabtn");
   const imageBanner = trayData?.image;
   const title = trayData?.title;
   const desc = trayData?.text;
   const buttonText = trayData?.button1_text;
   const buttonColor = "var(--color-primary)";
   const textColor = "var(--color-secondary)";
+  const route = trayData?.button_link;
 
-  const handleBtnClick = () => { };
+  const handleBtnClick = () => {};
 
-  return <section className={styles.mainCont}>
-    <section className={styles.textBlock}>
-      <h5 className={styles.title}>{title}</h5>
-      <p className={styles.desc}>{renderHTML(desc)}</p>
-      {buttonText && <section className={styles.btnSection}>
-        <FlameBtn color={buttonColor} text={buttonText} textColor={textColor} isLoadState={false} btnFunction={handleBtnClick} />
-      </section>}
-    </section>
-    <section className={styles.imageBlock}>
-      <FlameImage src={imageBanner} alt={'bannerImg'} />
-      {/* </section>
+  return (
+    <section className={styles.mainCont}>
+      <section className={styles.textBlock}>
+        <h5 className={styles.title}>{title}</h5>
+        <p className={styles.desc}>{renderHTML(desc)}</p>
+        {buttonText && (
+          <section className={styles.btnSection}>
+            <Link href={route}>
+              <FlameBtn
+                color={buttonColor}
+                text={buttonText}
+                textColor={textColor}
+                isLoadState={false}
+                btnFunction={handleBtnClick}
+              />
+            </Link>
+          </section>
+        )}
+      </section>
+      <section className={styles.imageBlock}>
+        <FlameImage src={imageBanner} alt={"bannerImg"} />
+        {/* </section>
         </section>
         <section className={styles.imageBlock}> */}
-      {/* <FlameImage src={imageBanner} alt={"bannerImg"} /> */}
+        {/* <FlameImage src={imageBanner} alt={"bannerImg"} /> */}
+      </section>
     </section>
-  </section>
+  );
 }
 export default ImageRightTextLeft;
