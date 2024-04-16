5ec75c96da851dc52059e339b135745cc481d60e
Chethan HP <chethan@cda.group>
2024-04-16 19:32:15 +0530

    cda clickable

--- src/components/Footer/CopyRightText/index.jsx
+++ src/components/Footer/CopyRightText/index.jsx
@@ -1,10 +1,13 @@
 import styles from "../footer.module.css";
+import Link from "next/link";
 function CopyRightText() {
   const currentYear = new Date().getFullYear();
   return (
     <section className={styles.copyRightCont}>
       <p> © Copyright © {currentYear} - Flame Pro</p>
-      <p>Designed and Developed by CDA</p>
+      <p>
+        Designed and Developed by <Link href={"https://cda.group/"}>CDA</Link>
+      </p>
     </section>
   );
 }
