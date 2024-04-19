fb1b099756b52db0b40aa6fdc98f9dc35b0c3ef2
Sai Nagarjuna <sai@cda.group>
2024-04-19 19:14:50 +0530

    resource hub filters ui

---
+++ src/components/ResourceHub/ResourceFilters/index.jsx
@@ -0,0 +1,84 @@
+import { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import FlameImage from '@/reusbleComponents/FlameImage';
+import FlameBtn from '@/reusbleComponents/FlameBtn';
+import styles from './resourceFilters.module.css';
+
+function ResourceFilters() {
+    const [mainCatFilter, setMainCatFilter] = useState('');
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState({})
+    const isDesktop = useMediaQuery({ query: "(min-width:900px)" });
+    const closeBtnSrc = '/Images/offCanvasclose.svg';
+    const fireBtncolor = mainCatFilter === 'Fire' ? "var(--color-primary)" : "var(--color-secondary)";
+    const fireTextClr = mainCatFilter === 'Fire' ? "var(--color-secondary)" : "var(--color-primary)";
+    const defenceBtncolor = mainCatFilter === 'Defence' ? "var(--color-primary)" : "var(--color-secondary)";
+    const defenceTextClr = mainCatFilter === 'Defence' ? "var(--color-secondary)" : "var(--color-primary)";
+    const upArrow = "/Images/upGreyArrow.svg";
+    const downArrow = "/Images/downArrowGrey.svg";
+    const filterData = [{ name: 'Select Type', filterList: ['Blogs', 'Videos', 'Downloads', 'Case Studies'] }, { name: 'Select Application', filterList: ['HVP', 'Structural', 'Technical Rescue', 'Wild Fire'] }, { name: 'Select Industry', filterList: ['HVP', 'Structural', 'Technical Rescue', 'Wild Fire'] }];
+
+    const handleMainCatSel = (selectedCat) => {
+        setMainCatFilter(selectedCat);
+    }
+
+    const handleSelectedCategory = (selectedCat) => {
+        if (selectedCategory?.name === selectedCat?.name) {
+            setIsDropdownOpen(false);
+            setSelectedCategory({});
+            return
+        }
+        setIsDropdownOpen(true);
+        setSelectedCategory(selectedCat);
+    }
+
+    return <section className={styles.mainCont}>
+        <section className={styles.headSection}>
+            <h3 className={styles.heading}>Filter By</h3>
+            <section className={styles.headBtnSec}>
+                <section className={styles.iconSty}>
+                    <FlameImage src={closeBtnSrc} alt='clear' />
+                </section>
+                <p className={styles.clearTxt}>clear</p>
+            </section>
+        </section>
+        <section className={styles.filterList}>
+            <section className={styles.fireDefFilter}>
+                <FlameBtn color={fireBtncolor}
+                    text={"Fire"}
+                    textColor={fireTextClr}
+                    isLoadState={false}
+                    btnFunction={() => { handleMainCatSel('Fire') }}
+                    isFromContactForm
+                    isSmallBtn={isDesktop ? false : true}
+                />
+                <FlameBtn color={defenceBtncolor}
+                    text={"Defence"}
+                    textColor={defenceTextClr}
+                    isLoadState={false}
+                    btnFunction={() => { handleMainCatSel('Defence') }}
+                    isFromContactForm
+                    isSmallBtn={isDesktop ? false : true}
+                />
+            </section>
+            <section className={styles.allCatFilterSec}>
+                {filterData?.map((mainCat) => (
+                    <section className={styles.categoryAndDropdown}>
+                        <section className={styles.filterDropDown} onClick={() => { handleSelectedCategory(mainCat) }}>
+                            <h3 className={styles.mainCatText}>{mainCat?.name}</h3>
+                            <section className={styles.downArrow}>
+                                <FlameImage src={selectedCategory?.name === mainCat?.name ? upArrow : downArrow} alt="arrow" />
+                            </section>
+                        </section>
+                        {isDropdownOpen && selectedCategory?.name === mainCat?.name && <section className={styles.dropDownBlock}>
+                            {mainCat?.filterList.map((cat) => {
+                                return <h4 className={styles.innerCatSec}>{cat}</h4>
+                            })}
+                        </section>}
+                    </section>
+                ))}
+            </section>
+        </section>
+    </section>
+}
+export default ResourceFilters
--- src/components/ResourceHub/ResourceHubListing/index.jsx
+++ src/components/ResourceHub/ResourceHubListing/index.jsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
 import TitleAndTextCard from '@/components/Cards/TitleAndTextCard';
+import ResourceFilters from '../ResourceFilters';
 import styles from '../resourceHub.module.css';
 
 function ResourceHubListing({ listingData }) {
 
     return <section className={styles.mainCont}>
+        <section className={styles.filterCont}>
+            <ResourceFilters />
+        </section>
         <section className={styles.products}>
             {listingData?.map((product, index) => {
                 return <TitleAndTextCard key={index} data={product} />
