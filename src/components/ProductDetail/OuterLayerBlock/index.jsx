9f4d305c63c88ab9ae7695ef4ec74bb5707b184b
Sai Nagarjuna <sai@cda.group>
2024-04-27 14:54:58 +0530

    product detail page popup

--- src/components/ProductDetail/OuterLayerBlock/index.jsx
+++ src/components/ProductDetail/OuterLayerBlock/index.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import { renderHTML } from '@/utils/htmlString';
+import FlameImage from '@/reusbleComponents/FlameImage';
 import styles from './outerLayerBlock.module.css';
 
 function OuterLayerBlock({ productInfo }) {
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const [popupContent, setPopupContent] = useState('');
     const data = productInfo?.filter((e) => e?.title === 'Outer Layers');
     const info = data?.length > 0 ? data[0] : {};
+    const offCanvasClose = '/Images/offCanvasClose.svg';
     const outerLayerItems = info?.outer_layer_item;
     const headingsArr = ['', 'Colour Availability', 'Outer Shell Tensile/Tear Strength', 'Radiant Heat Transfer Performance', 'Breathability Performance', 'Lead Times', 'Notes'];
+
+    const closePopup = () => {
+        setIsPopupOpen(false);
+    }
+
+    const handleHeadingClick = (item) => {
+        setIsPopupOpen(true);
+        setPopupContent(item);
+    }
 
     return (
         <div className={styles.outerLayerTable}>
@@ -21,7 +35,7 @@ function OuterLayerBlock({ productInfo }) {
                     <tbody>
                         {outerLayerItems?.map((item, index) => (
                             <tr key={index} className={index % 2 === 0 ? styles.brownRow : ''}>
-                                <td><u>{item.title_outer_layer}</u></td>
+                                <td onClick={() => { handleHeadingClick(item) }} role='button'><u>{item.title_outer_layer}</u></td>
                                 <td>{item.colour_availability}</td>
                                 <td>{item.outer_shell_tensile__tear_strength}</td>
                                 <td>{item.radiant_heat_transfer_performance}</td>
@@ -33,6 +47,24 @@ function OuterLayerBlock({ productInfo }) {
                     </tbody>
                 </table>
             </div>
+            {isPopupOpen && <div className={styles.popupBackground} onClick={closePopup}>
+                <div
+                    className={styles.popupContent}
+                    onClick={(e) => e.stopPropagation()}
+                >
+                    <section className={styles.popupCont}>
+                        <section className={styles.headingSec}>
+                            <h3 className={styles.popupHeading}>{popupContent?.title_outer_layer}</h3>
+                            <section className={styles.closeBtnSec}>
+                                <figure className={styles.closeBtn} onClick={closePopup}>
+                                    <FlameImage src={offCanvasClose} alt='closeBtn' />
+                                </figure>
+                            </section>
+                        </section>
+                        <p className={styles.popupText}>{renderHTML(popupContent?.popup_text)}</p>
+                    </section>
+                </div>
+            </div>}
         </div>
     );
 }
