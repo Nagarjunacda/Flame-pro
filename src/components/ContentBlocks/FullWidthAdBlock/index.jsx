4a00f7860d3db8b5ab052593811de633cbe2774b
Sai Nagarjuna <sai@cda.group>
2024-04-29 22:59:43 +0530

    minor changes for full width ad block

--- src/components/ContentBlocks/FullWidthAdBlock/index.jsx
+++ src/components/ContentBlocks/FullWidthAdBlock/index.jsx
@@ -7,6 +7,7 @@ import { renderHTML } from "@/utils/htmlString";
 import styles from "./fullWidthAdBlock.module.css";
 
 function FullWidthAdBlock({ trayData }) {
+  console.log(trayData, '!!')
   const router = useRouter();
   const { footerContextData } = useFooterContextData();
   const globalAddress = footerContextData?.acf?.footer_contact_address;
@@ -18,7 +19,7 @@ function FullWidthAdBlock({ trayData }) {
   const phoneNum = footerContextData?.acf?.phone_no;
   const isContactUsPage = router?.asPath === '/contact-us';
   const imgSrc = trayData?.image;
-  const text = trayData?.enable_address ? globalAddTrimed : trayData?.text;
+  const text = trayData?.enable_address === 'Yes' ? globalAddTrimed : trayData?.text;
   const buttonTitle = trayData?.button_title;
   const title = trayData?.title;
   const buttonColor = "var(--color-secondary)";
@@ -41,7 +42,7 @@ function FullWidthAdBlock({ trayData }) {
           <h3 className={styles.title}>{title}</h3>
           <section className={styles.descSection}>
             <h5 className={styles.desc}>{renderHTML(text)}</h5>
-            {trayData?.enable_email_and_phone &&
+            {trayData?.enable_email_and_phone === 'Yes' &&
               <section className={styles.infoSection}>
                 <a href={`mailto:${emailAdd}`} className={styles.contactInfo}>
                   <h5 className={styles.contactInfo}>{emailAdd}</h5>
